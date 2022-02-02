b7a02816ae125661879475bdc66c57963ad3f50b
Wenwen Tian <wwtian9@gmail.com>
2022-02-02 15:33:20 -0700

    add get and post router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,34 @@
-// create 3 routes: GET, POST, and delete notes
+const fs = require("fs");
+// 'uuid' npm package for unique id:
+const { v4: uuidv4 } = require("uuid");
 
 // express router
+const router = require("express").Router();
+
+// imports notes
+let notes = require("../data/db.json");
+
+//GET notes
+router.get("/notes", (req, res) => {
+  res.json(notes).catch((err) => res.status(500).json(err));
+});
+
+//create notes
+router.post("/notes", (req, res) => {
+  req.body.id = uuidv4();
+  notes.push(req.body);
+
+  fs.writeFileSync("../db/db.json", JSON.stringify(notes));
+  res.json(notes);
+});
+
+//DELETE notes
+
+module.exports = router;
+
+// write notes, read notes, removing notes
+// fs funtion, fs.readfile , fs.writefile,
+// makes normal function to a promise: util.promisify
+
+// write a method to remove db.json file,
+// create a class called STORE: read write get add removeb (5 methods)
--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-// create server in this file 
+// create express server
+const express = require("express");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// get routes
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Use apiRoutes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
