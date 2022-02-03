e250cf512daefbeeca5f908f032313286a528257
Wenwen Tian <wwtian9@gmail.com>
2022-02-02 23:54:19 -0700

    fixed DELETE issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,24 +2,18 @@
 const router = require("express").Router();
 const fs = require("fs");
 
-// 'uuid' npm package for unique id:
-// const { v4: uuidv4 } = require("uuid");
-
 // imports notes
-// let notes = require("../data/db.json"); REQUIRE ONLY READ ONCE
+let notes = require("../data/db.json");
+let id = notes.length + 1;
 
 //GET notes
 router.get("/notes", (req, res) => {
-  let notes = JSON.parse(fs.readFileSync("./data/db.json", "utf8"));
   res.json(notes);
 });
 
 //create notes
 router.post("/notes", (req, res) => {
-  let notes = JSON.parse(fs.readFileSync("./data/db.json", "utf8"));
-  let id = notes.length + 1;
   req.body.id = id;
-  // req.body.id = uuidv4();
   notes.push(req.body);
 
   fs.writeFileSync("./data/db.json", JSON.stringify(notes));
@@ -28,12 +22,10 @@ router.post("/notes", (req, res) => {
 
 //DELETE notes
 router.delete("/notes/:id", (req, res) => {
-  let id = req.params.id;
-  let notes = JSON.parse(fs.readFileSync("./data/db.json", "utf8"));
   let newNotes = notes.filter((note) => note.id !== id);
-
+  notes = newNotes;
   fs.writeFileSync("./data/db.json", JSON.stringify(newNotes));
-  res.end();
+  res.json(true);
 });
 
 module.exports = router;
